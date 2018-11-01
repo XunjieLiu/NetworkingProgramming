b277eb2de7eeb15683111e04a33ee666bc730f51
Xunjie <xunjieliu97@gmail.com>
2018-11-01 22:50:27 +0800

    Introduction to project 2

---
+++ Crawler/imgeCrawler.py
@@ -0,0 +1,15 @@
+'''
+作业描述： 
+2 input parameters: URL, depth(optional, default to 5)
+
+不允许使用库
+
+使用URL为名字新建一个文件夹　存放图片
+
+URL可能是/path/path/path的，为每个path建立一个文件夹，基本上URL有多少层文件夹就有多少层
+
+下载所有image, 并命名
+
+打扰了 这是project2， 我说怎么会这么难
+
+'''
